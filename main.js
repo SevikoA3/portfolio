29555bf0312f93bf3ed52dfca1ab0b176932b93f
SevikoA3 <31793124+SevikoA3@users.noreply.github.com>
2023-09-18 21:55:47 +0700

    fix: typo

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const text = document.querySelector(".multiText");
-    const text1Cont = document.querySelector(".text1");
-    const text2Cont = document.querySelector(".helloworld");
+const text = document.querySelector(".multiText");
+const text1Cont = document.querySelector(".text1");
+const text2Cont = document.querySelector(".helloworld");
 
-    const textLoad = () => {
-        setTimeout(() => {
-            text.textContent = "Programmer.";
-        }, 0);
-        setTimeout(() => {
-            text.textContent = "Photographer.";
-        }, 4000);
-        setTimeout(() => {
-            text.textContent = "Videographer.";
-        }, 8000);
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Programmer.";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Photographer.";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Videographer.";
+    }, 8000);
+}
+
+textLoad();
+setInterval(textLoad, 12000);
+
+function myFunction(x) {
+    if (x.matches) { 
+        text1Cont.innerHTML = "My name is <b>Seviko Attalarik</b> and<br>I'm a ";
+        text2Cont.innerHTML = "Hello, World!";
+    }else {
+        text1Cont.innerHTML = "My name is <b>Seviko Attalarik</b> and I'm a<br> ";
+        text2Cont.innerHTML = "Hello,<br>World!";
     }
+}
 
-    textLoad();
-    setInterval(textLoad, 12000);
-
-    function myFunction(x) {
-        if (x.matches) { 
-            text1Cont.innerHTML = "My name is <b>Seviko Attalarik</b> and<br>I'm a ";
-            text2Cont.innerHTML = "Hello, World!";
-        }else {
-            text1Cont.innerHTML = "My name is <b>Seviko Attalarik</b> and I'm a<br> ";
-            text2Cont.innerHTML = "Hello,<br>World!";
-        }
-    }
-    
-    var x = window.matchMedia("(min-width: 1137px)");
-    myFunction(x);
-    x.addListener(myFunction);
-});
+var x = window.matchMedia("(min-width: 1137px)");
+myFunction(x);
+x.addListener(myFunction);
